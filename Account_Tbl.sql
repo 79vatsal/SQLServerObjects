e465bd5f012384d407a18403364a6001d6f34f34
79vatsal <51381785+79vatsal@users.noreply.github.com>
2020-08-17 22:13:48 -0400

    Add files via upload

--- Account_Tbl.sql
+++ Account_Tbl.sql
@@ -1,15 +1,10 @@
-/****** Object:  Table [dbo].[Account]    Script Date: 8/17/2020 11:37:55 AM ******/
 IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Account]') AND type in (N'U'))
 DROP TABLE [dbo].[Account]
 GO
-
-/****** Object:  Table [dbo].[Account]    Script Date: 8/17/2020 11:37:55 AM ******/
 SET ANSI_NULLS ON
 GO
-
 SET QUOTED_IDENTIFIER ON
 GO
-
 CREATE TABLE [dbo].[Account](
 	[ID] [nchar](10) NULL,
 	[Account_Number] [nchar](10) NULL
--- Branch_Tbl.sql
+++ Branch_Tbl.sql
@@ -1,15 +1,10 @@
-/****** Object:  Table [dbo].[Branch]    Script Date: 8/17/2020 11:39:52 AM ******/
 IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Branch]') AND type in (N'U'))
 DROP TABLE [dbo].[Branch]
 GO
-
-/****** Object:  Table [dbo].[Branch]    Script Date: 8/17/2020 11:39:52 AM ******/
 SET ANSI_NULLS ON
 GO
-
 SET QUOTED_IDENTIFIER ON
 GO
-
 CREATE TABLE [dbo].[Branch](
 	[ID] [nchar](10) NULL,
 	[Branch _Name] [nchar](10) NULL
